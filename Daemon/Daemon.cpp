c62627d01af1153ba3cafbbd39a77b9f305d661d
马冬亮 <mdl2009@vip.qq.com>
2013-01-27 10:48:21 +0800

    添加FreeJudger的守护程序工程
    此工程用于后台评判

---
+++ Daemon/Daemon.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Daemon.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+
+
+    return 0;
+}
